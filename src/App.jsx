b1a3a1570c5f264c1465aa1bdbdf51294e842821
Ademir Rivera <uarivera@innovacion.gob.sv>
2025-06-18 15:26:06 -0600

    feat: agregando cascaron para todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Button from './components/Button'
+import { useReducer } from "react";
+import { todoReducer } from "./hooks/todoReducer";
+
+const initialState = [{
+  id: new Date().getTime(),
+  name: 'Recolectar la piedra del Alma',
+  done: false,
+},
+{
+  id: new Date().getTime() + 100,
+  name: 'Recolectar la piedra del Test',
+  done: false,
+}];
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [todos, dispatch] = useReducer(todoReducer, initialState)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1> TodoApp (10) | <small> pendientes: </small>  </h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-7">
+
+          <ul className="list-group">
+            {
+              todos.map(todo => (
+                <li className="list-group-item d-flex justify-content-between" key={todo.id}>
+                  <span className="align-self-center"> {todo.name} </span>
+                  <button className="btn btn-danger" > Borrar </button>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4> Agregar TODO </h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder="¿Qué hay que hacer?"
+              className="form-control"
+            />
+            <button
+              type="submit"
+              className="btn btn-outline-primary my-1 btn-block"
+            >
+              Agregar
+            </button>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
